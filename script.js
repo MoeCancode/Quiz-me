c65de776fd41d5253dd7625fe69cbe96ecd3cc16
M A Razvi <abuzarflw@gmail.com>
2022-04-06 01:50:24 -0700

    Create timer function. Have it decrease 10 seconds when wrong answer is chosen. Display time score when quiz is over

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ var disclaimerBox = document.querySelector(".disclaimerBox");
 var questionBox = document.querySelector(".questionBox");
 var homeScreen = document.querySelector("#homeScreen");
 var resultBox = document.querySelector(".result");
+var countdownTimer = document.querySelector(".timeLeft p span");
+var scoreDisplay = document.querySelector("#myScore span");
+
+var timeScore = 60;
+
+var optionOne = document.querySelector("#one");
+var optionTwo = document.querySelector("#two");
+var optionThree = document.querySelector("#three");
+var optionFour = document.querySelector("#four");
 
 // When start Button is clicked 
 startButton.addEventListener("click", function(){
@@ -16,10 +25,11 @@ goButton.addEventListener("click", function(){
     disclaimerBox.classList.remove("triggerDisclaimer");
     questionBox.classList.add("triggerQuestionBox");
     displayQuestions(0);
+    startCountdown(60);
  })
 
-
  var startingQuestionCount = 0;
+ var counter;
  var nextButton = document.querySelector(".footer button")
 
  //Click Next question to update value of num
@@ -36,7 +46,10 @@ goButton.addEventListener("click", function(){
      
      else {
          questionBox.classList.remove("triggerQuestionBox");
+         clearInterval(counter);
+         scoreDisplay.innerHTML = timeScore; 
          resultBox.classList.add("triggerResultBox");
+         
 
      }
  })
@@ -44,21 +57,23 @@ goButton.addEventListener("click", function(){
   
  //function that displays questions
     function displayQuestions(num) {
-
-        var optionOne = document.querySelector("#one");
-        var optionTwo = document.querySelector("#two");
-        var optionThree = document.querySelector("#three");
-        var optionFour = document.querySelector("#four");
         
         //Reset select styling for all options
         optionOne.style.backgroundColor = "rgb(253, 187, 143)";
         optionOne.style.color = "black";
+        optionOne.style.pointerEvents = "auto";
+
         optionTwo.style.backgroundColor = "rgb(253, 187, 143)";
         optionTwo.style.color = "black";
+        optionTwo.style.pointerEvents = "auto";
+
         optionThree.style.backgroundColor = "rgb(253, 187, 143)";
         optionThree.style.color = "black";
+        optionThree.style.pointerEvents = "auto";
+
         optionFour.style.backgroundColor = "rgb(253, 187, 143)";
         optionFour.style.color = "black";
+        optionFour.style.pointerEvents = "auto";
 
         
         //Update question number
@@ -101,7 +116,28 @@ goButton.addEventListener("click", function(){
       else {
           myAnswer.style.backgroundColor = "red";
           myAnswer.style.color = "white";
+          timeScore = timeScore - 10;
+          clearInterval(counter);
+          countdownTimer.innerHTML = timeScore;
+          startCountdown(timeScore);
       }
+
+      //Disable selection of options after one is selected
+      optionOne.style.pointerEvents = "none";
+      optionTwo.style.pointerEvents = "none";
+      optionThree.style.pointerEvents = "none";
+      optionFour.style.pointerEvents = "none";
+
+  }
+
+  //Function that starts the timer
+
+  function startCountdown(time) {
+    counter = setInterval(function() {
+        countdownTimer.textContent = time;
+        timeScore--;
+        time--;
+    }, 1000)
   }
 
 
